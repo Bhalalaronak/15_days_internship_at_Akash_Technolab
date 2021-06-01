00c834eada455e34b4741b45da94a010c4e7c680
Bhalala Ronakbhai Bharatbhai <73635050+Bhalalaronak@users.noreply.github.com>
2021-06-01 23:44:16 +0530

    Add files via upload

---
+++ Task6/task2/niraj.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+<h1 align="center"><b>Task : 6</b></h4>
+	<h1 align="center"><b>Insertion operation with theme</b></h4>
+	<div>
+<form action="niraj1.php" method="post" align="center">
+
+
+  <label for="fnamide">Id :</label>
+  <input type="text" id="id" name="id"><br><br>
+
+
+  <label for="name" style="margin_top:10px;";>Name:</label>
+  <input type="text" id="name" name="name"><br><br>
+
+  <label>Gender:</label>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label><br><br>
+
+  <label for="date">DOB:</label>
+  <input type="date" id="date" name="b_date"><br><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email"><br><br>
+
+  <label for="number">Mobile Number:</label>
+  <input type="text" id="number" name="number"><br><br>
+
+  <label for="pass">Password:</label>
+  <input type="password" id="pass" name="pass"><br><br>
+
+  <label for="area">Area:</label>
+  <input type="text" id="area" name="area"><br><br>
+
+  <label>Address:</label><br>
+  <textarea name="address" rows="10" cols="30">
+  </textarea><br><br>
+
+  <label for="pincode">Pincode:</label>
+  <input type="text" id="pincode" name="pincode"><br><br>
+
+  <label for="blodgroup">Blodgroup:</label>
+  <select id="blodgroup" name="blodgroup">
+  <option value="">Select blodgroup</option>
+  <option value="A+">A+</option>
+  <option value="B+">B+</option>
+  <option value="O+">O+</option>
+  <option value="O-">O-</option>
+  </select><br><br>
+
+  <input type="submit" value="Submit" name="submit">
+
+</form>
+</div>
+</body>
+</html>
