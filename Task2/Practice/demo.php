2ba3ded75782308a529e5f28f4518adf17f3180b
Bhalala Ronakbhai Bharatbhai <73635050+Bhalalaronak@users.noreply.github.com>
2021-06-03 20:39:16 +0530

    Add files via upload

---
+++ Task2/Practice/demo.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "Welcome To PHP";
+    echo "<h1>I am h1 in PHP</h1>";
+
+?>
+
+<html>
+    <body>
+        Welcome to HTML code
+        <?php
+            echo "I am PHP inside HTML";
+        ?>
+    </body>
+</html>
---
+++ Task2/Practice/stringfunction.php
@@ -0,0 +1,13 @@
+<?php
+    echo "<h1>String Length</h1>";
+    echo strlen("Hello world!");
+
+    echo "<h1>Word Count</h1>";
+    echo str_word_count("Hello world!");
+
+    echo "<h1>String Reverse</h1>";
+    echo strrev("Hello world!");
+
+    echo "<h1>String Replace</h1>";
+    echo str_replace("world", "Ronak", "Hello world!");
+?> 
